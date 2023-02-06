6aa2e351e7c2bf9319029335efb029e610321ec6
Kit Bolourchi <kitbolourchi@Kit-Bolourchis-MacBook-Air.local>
2023-02-06 18:36:28 +0000

    Added speaker controller

--- src/main/java/com/example/demo/controllers/SessionsController.java
+++ src/main/java/com/example/demo/controllers/SessionsController.java
@@ -2,7 +2,8 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.Session;
 import com.example.demo.repositories.SessionRepository;
-import com.example.demo.repositories.SpeakerRepository;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,5 +29,23 @@ public class SessionsController {
     @PostMapping
     public Session create(@RequestBody Session session) {
         return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable long id) {
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public void update(@PathVariable long id, @RequestBody Session session) {
+        try {
+            Session existingSession = sessionRepository.getReferenceById(id);
+            BeanUtils.copyProperties(session, existingSession, "session_id");
+            sessionRepository.saveAndFlush(existingSession);
+        } catch (Exception e) {
+            System.out.printf("Error: {}", e.getMessage());
+        }
+
+
     }
 }
---
+++ src/main/java/com/example/demo/controllers/SpeakersController.java
@@ -0,0 +1,48 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Session;
+import com.example.demo.models.Speaker;
+import com.example.demo.repositories.SessionRepository;
+
+import com.example.demo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/ve/speakers")
+public class SpeakersController {
+
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list() {
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return speakerRepository.getReferenceById(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody Speaker speaker) {
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable long id) {
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public void update(@PathVariable long id, @RequestBody Speaker speaker) {
+        Speaker existingSpeaker = speakerRepository.getReferenceById(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        speakerRepository.saveAndFlush(existingSpeaker);
+    }
+}
